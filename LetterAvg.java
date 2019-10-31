0f04dd6db0fef1d98c69a1e87fe1bc9658f74546
jia <jiawei.huang-1@ou.edu>
2019-10-31 00:18:30 -0500

    added numberOfStationWithLetterAvg method

--- LetterAvg.java
+++ LetterAvg.java
@@ -1,9 +1,25 @@
 
 public class LetterAvg {
-
-	public static void main(String[] args) {
+	private char letterAverage;
+	public LetterAvg(char letterAverage) {
+		readFile();
+		this.letterAverage = letterAverage;
 		
-
+	}
+	
+	public int numberOfStationWithLetterAvg() {
+		String a = String.valueOf(letterAverage);
+		
+		int count = 0;
+		for(int i = 0; i < list.size();++i) {
+			char str = list.get(i).charAt(0);
+			String b = String.valueOf(str);
+			if(a.equals(b)) {
+				count++;
+			}
+		}
+		return count;
+		
 	}
 
 }
