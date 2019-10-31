aedd60b6380153a32effe058dfe7287b28684331
jia <jiawei.huang-1@ou.edu>
2019-10-31 00:41:46 -0500

    add some method

--- PosAvg.java
+++ PosAvg.java
@@ -1,23 +1,54 @@
 
-public class PosAvg {
+public class PosAvg extends MesoInherit{
 
 	private String stID;
+	private Object stationMinus1;
+	private Object stationMinus2;
+	private Object stationPlus1;
+	private Object stationPlus2;
 
 	public PosAvg(String stID) {
 		this.stID = stID;
+		readStations();
+		
+		
 		// TODO Auto-generated constructor stub
 	}
-
+	public int indexOfStation() {
+		
+		int index = 0;
+		for(int i = 0; i < list.size(); ++i) {
+			if(stID.equals(list.get(i)))
+			{
+				index = i;
+			}
+		}
+		
+		otherStations(index);
+		return index;
+	}
 	
-	
-	public int findIndex() {
+	public void otherStations(int index)
+	{
 		
+		if (index > 1 && index < 120) 
+		{
+		this.stationMinus1 = list.get(index - 1);
+		this.stationMinus2 = list.get(index - 2);
 		
-		
-		return 0;
-		
-		
+		this.stationPlus1 = list.get(index + 1);
+		this.stationPlus2 = list.get(index + 2);
+		}
+	}
+		public String toString() {
+			indexOfStation();
+		return String.format("This index is average of %s and %s, %s and %s, and so on.", stationMinus1,
+				stationPlus1, stationMinus2 ,
+				stationPlus2);
+	}
 		
 	}
+	
+	
 
-}
+
