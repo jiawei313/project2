22c95684aad4d4c95d8cbd9ba20c213d625d1bae
jia <jiawei.huang-1@ou.edu>
2019-10-31 00:59:04 -0500

    add startingWithLetter method

--- LetterAvg.java
+++ LetterAvg.java
@@ -15,7 +15,7 @@ public class LetterAvg extends MesoInherit{
 		String a = String.valueOf(letterAverage);
 		
 		int count = 0;
-		for(int i = 0; i < list.size();++i) {
+		for(int i = 0; i < list.size(); ++i) {
 			char str = list.get(i).charAt(0);
 			String b = String.valueOf(str);
 			if(a.equals(b)) {
