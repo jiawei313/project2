4f5a56d345271a756879323fda686896f9727fb3
jia <jiawei.huang-1@ou.edu>
2019-10-31 00:15:10 -0500

    Initial commit

---
+++ PosAvg.java
@@ -0,0 +1,23 @@
+
+public class PosAvg {
+
+	private String stID;
+
+	public PosAvg(String stID) {
+		this.stID = stID;
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	public int findIndex() {
+		
+		
+		
+		return 0;
+		
+		
+		
+	}
+
+}
