396e1e98779bbe6409d222ed8638471cc54be9af
jia <jiawei.huang-1@ou.edu>
2019-10-31 00:19:43 -0500

    added a new method

--- LetterAvg.java
+++ LetterAvg.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class LetterAvg {
 	private char letterAverage;
@@ -20,6 +21,23 @@ public class LetterAvg {
 		}
 		return count;
 		
+	}
+	
+	public ArrayList<String> startingWithLetter() {
+		String a = String.valueOf(letterAverage);
+		ArrayList<String> Arrlist = new ArrayList<String>();
+		
+		for(int i = 0; i < list.size();++i) {
+			char str = list.get(i).charAt(0);
+			String b = String.valueOf(str);
+			if(a.equals(b)) {
+				
+				Arrlist.add(list.get(i));
+				
+			}
+		}
+		
+		return Arrlist;
 	}
 
 }
